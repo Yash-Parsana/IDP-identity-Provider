b1f59366861ecc3278d797a72461e365ae6af870
Dharmesh Vala <dvala453@gmail.com>
2023-10-06 16:11:49 +0530

    changes by dharmesh

--- Middleware/verifyToken.js
+++ Middleware/verifyToken.js
@@ -1,19 +1,24 @@
 const jwt = require('jsonwebtoken')
 
 //This Middleware verify JWT token
-const verify=async(req,res,next) => {
+const verify=async (req,res,next) => {
     
     try {
-        
+        // console.log(req.cookie)
+        // const token = req.cookies.access_token;
         const token = req.cookies.jwtToken;
+        
+        console.log("token in verify file -   " +token);
         const obj =await jwt.verify(token, process.env.jwtKEY);
+        console.log(obj);
         req.body.ClientID=obj.ClientID
         
         next()
     }
     catch (err)
     {
-        res.status(498).json({
+        console.log(err)
+        res.status(555).json({
             message:err
         })
     }
